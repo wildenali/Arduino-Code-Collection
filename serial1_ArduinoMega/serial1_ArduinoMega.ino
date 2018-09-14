d8261ca3e3434d244b1dfdfb675282a6b950a269
Wilden Ali <wildeeeeen@gmail.com>
2018-09-14 16:44:48 +0700

    Sending data to hoverboard
    use serial1 to send data

---
+++ serial1_ArduinoMega/serial1_ArduinoMega.ino
@@ -0,0 +1,15 @@
+int16_t steer;
+int16_t gas;
+
+
+
+void setup() {
+  Serial1.begin(19200);
+}
+
+void loop(void) {
+  steer = 0;//map(analogRead(potA), 0, 1023, -100, 100);
+  gas = 100;//map(analogRead(potB), 0, 1023, -100, 100);
+  Serial1.write((uint8_t *) &steer, sizeof(steer));
+  Serial1.write((uint8_t *) &gas, sizeof(gas));
+}
