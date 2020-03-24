cb6bef4a7fac89392895af665b06c52c745f091c
Wilden Ali <wildeeeeen@gmail.com>
2020-03-24 23:14:59 +0700

    ada return nya

---
+++ StateChangeButton.ino
@@ -0,0 +1,39 @@
+const int ledPin = 25;       // the pin that the LED is attached to
+const int buttonPin = 15;    // the pin that the pushbutton is attached to
+bool lastTombolState = 0;
+
+void setup() {
+  
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(115200);
+}
+
+
+void loop() {
+  
+  Serial.println(tombol(buttonPin));
+
+}
+
+bool tombol(const int pin){
+  bool buttonState;
+  bool buttonPushCounter = false;
+  buttonState = digitalRead(pin);
+  if (buttonState != lastTombolState) {
+    if (buttonState == HIGH){
+      buttonPushCounter = true;
+      Serial.println("cek");
+    }
+  }
+  lastTombolState = buttonState;
+
+  if (buttonPushCounter == true) {
+    buttonPushCounter = false;
+    return true;
+  }else{
+    return false;
+  }
+  
+}
+
